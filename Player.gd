6f287876c0492748692ae8a47035c4233c589592
Heartbeast <heartbeast.studios@gmail.com>
2022-07-13 13:05:48 -0600

    Second video done

--- Player.gd
+++ Player.gd
@@ -1,17 +1,42 @@
 extends KinematicBody2D
 
+export(int) var JUMP_FORCE = -130
+export(int) var JUMP_RELEASE_FORCE = -70
+export(int) var MAX_SPEED = 50
+export(int) var ACCELERATION = 10
+export(int) var FRICION = 10
+export(int) var GRAVITY = 4
+export(int) var ADDITIONAL_FALL_GRAVITY = 4
+
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
-	velocity.y += 4
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = 50
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -50
+	apply_gravity()
+	var input = Vector2.ZERO
+	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	
+	if input.x == 0:
+		apply_friction()
 	else:
-		velocity.x = 0
+		apply_acceleration(input.x)
 	
-	if Input.is_action_just_pressed("ui_up"):
-		velocity.y = -120
+	if is_on_floor():
+		if Input.is_action_pressed("ui_up"):
+			velocity.y = JUMP_FORCE
+	else:
+		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_RELEASE_FORCE:
+			velocity.y = JUMP_RELEASE_FORCE
+		
+		if velocity.y > 0:
+			velocity.y += ADDITIONAL_FALL_GRAVITY
 	
-	velocity = move_and_slide(velocity)
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+func apply_gravity():
+	velocity.y += GRAVITY
+
+func apply_friction():
+	velocity.x = move_toward(velocity.x, 0, FRICION)
+
+func apply_acceleration(amount):
+	velocity.x = move_toward(velocity.x, MAX_SPEED * amount, ACCELERATION)
