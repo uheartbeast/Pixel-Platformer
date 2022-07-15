d7c77d8d71214ca9747398818dadba0b72739e0a
Heartbeast <heartbeast.studios@gmail.com>
2022-07-15 09:11:17 -0600

    Finished video 3

--- Player.gd
+++ Player.gd
@@ -10,6 +10,8 @@ export(int) var ADDITIONAL_FALL_GRAVITY = 4
 
 var velocity = Vector2.ZERO
 
+onready var animatedSprite = $AnimatedSprite
+
 func _physics_process(delta):
 	apply_gravity()
 	var input = Vector2.ZERO
@@ -17,20 +19,33 @@ func _physics_process(delta):
 	
 	if input.x == 0:
 		apply_friction()
+		animatedSprite.animation = "Idle"
 	else:
 		apply_acceleration(input.x)
+		animatedSprite.animation = "Run"
+		
+		if input.x > 0:
+			animatedSprite.flip_h = true
+		elif input.x < 0:
+			animatedSprite.flip_h = false
 	
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
 			velocity.y = JUMP_FORCE
 	else:
+		animatedSprite.animation = "Jump"
 		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_RELEASE_FORCE:
 			velocity.y = JUMP_RELEASE_FORCE
 		
 		if velocity.y > 0:
 			velocity.y += ADDITIONAL_FALL_GRAVITY
 	
+	var was_in_air = not is_on_floor()
 	velocity = move_and_slide(velocity, Vector2.UP)
+	var just_landed = is_on_floor() and was_in_air
+	if just_landed:
+		animatedSprite.animation = "Run"
+		animatedSprite.frame = 1
 
 func apply_gravity():
 	velocity.y += GRAVITY
