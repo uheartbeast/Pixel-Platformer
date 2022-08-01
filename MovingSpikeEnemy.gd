41dd41f1caaee9847001cdc700acee265e62e0d1
Matthew Murphy <mashumafi@users.noreply.github.com>
2022-08-01 18:50:24 -0400

    An easier way to update a tools

--- MovingSpikeEnemy.gd
+++ MovingSpikeEnemy.gd
@@ -9,22 +9,25 @@ enum ANIMATION_TYPE {
 export(ANIMATION_TYPE) var animation_type setget set_animation_type
 export(int) var speed = 1 setget set_speed
 
-onready var animationPlayer: = $AnimationPlayer
+onready var animationPlayer := $AnimationPlayer as AnimationPlayer
 
 func set_animation_type(value):
 	animation_type = value
-	var ap = find_node("AnimationPlayer")
-	if ap: play_updated_animation(ap)
+	_update_animation()
 
 func set_speed(value):
 	speed = value
-	var ap = find_node("AnimationPlayer")
-	if ap: ap.playback_speed = speed
+	_update_animation()
 
 func _ready():
-	play_updated_animation(animationPlayer)
+	_update_animation()
 
-func play_updated_animation(ap):
+func _update_animation():
+	if not animationPlayer:
+		return
+
+	animationPlayer.playback_speed = speed
+
 	match animation_type:
-		ANIMATION_TYPE.LOOP: ap.play("MoveAlongPathLoop")
-		ANIMATION_TYPE.BOUNCE: ap.play("MoveAlongPathBounce")
+		ANIMATION_TYPE.LOOP: animationPlayer.play("MoveAlongPathLoop")
+		ANIMATION_TYPE.BOUNCE: animationPlayer.play("MoveAlongPathBounce")
